160b00d6992700764935446ba860d8c124245914
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-04-05 12:58:14 +0300

    variables comp

---
+++ 3_variables/RuleOne.java
@@ -0,0 +1,7 @@
+public class RuleOne{
+    public static void main(String [] args){
+        int numberOfDays = 7;
+        String opening_hours = "24 hours";
+        System.out.println("We are open " + opening_hours + ", " + numberOfDays + " days in a week");
+    }
+}
---
+++ 3_variables/RuleThree.java
@@ -0,0 +1,10 @@
+public class RuleThree{
+    static final String PHONE_NUMBER = "0700123456";
+    
+    public static void main(String [] args){
+        int numberOfDays = 7;
+        String opening_hours = "24 hours";
+        System.out.println("We are open " + opening_hours + ", " + numberOfDays + " days in a week");
+        System.out.println("Call us on: " + PHONE_NUMBER);
+    }
+}
