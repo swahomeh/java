93581e53b10d6d11eb72d29b65a97967f2263bdd
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-04-05 13:27:07 +0300

    objects

---
+++ 4_objects/NamesMain.java
@@ -0,0 +1,9 @@
+public class NamesMain {
+    public static void main(String[] args) {
+ 	   NamePrinter namePrinter = new NamePrinter();
+ 	   SurnamePrinter surnamePrinter = new SurnamePrinter();
+ 	  
+ 	   namePrinter.print();
+ 	   surnamePrinter.print();
+    }
+}
