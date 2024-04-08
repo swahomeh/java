1ebd0e6b79582fc1653e60124c4aba77e07652f0
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-04-08 12:35:05 +0300

    numerictypes

---
+++ 9_numerictypes/CharTest.java
@@ -0,0 +1,7 @@
+public class CharTest{
+    public static void main(String [] args){
+	    long myChar = 1 + '4'; // single quotes for char
+		
+		System.out.println(myChar);
+	}
+}
---
+++ 9_numerictypes/FloatTest.java
@@ -0,0 +1,8 @@
+public class FloatTest{
+    public static void main(String [] args){
+	    double myDouble = 2.4D; // defaults to double even without the D suffix
+		float myFloat = 2.9F; // to explicitly set a Float, use the F suffix
+		
+		System.out.println(myFloat);
+	}
+}
