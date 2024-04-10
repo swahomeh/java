81f8d1b0ecda3ee68ec2ac983d87eb8f73a3ab6a
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-04-10 16:11:59 +0300

    literals

--- 15_literals/LiteralSumHex.java
+++ 15_literals/LiteralSumHex.java
@@ -16,13 +16,13 @@ public class LiteralSumHex {
 		Scanner scanner = new Scanner(System.in);
 		LiteralSumHex obj = new LiteralSumHex();
 
-		int num1 = obj.num1(scanner);
-		int num2 = obj.num2(scanner);
+		String num1 = Integer.toHexString(obj.num1(scanner));
+		String num2 = Integer.toHexString(obj.num2(scanner));
 
-		int result = num1 * num2;
+		int result = Integer.parseInt(num1, 16) * Integer.parseInt(num2, 16);
 
-        System.out.println("\nHexadecimal num1: 0x" + Integer.toHexString(num1));
-        System.out.println("Hexadecimal num2: 0x" + Integer.toHexString(num2));
+        System.out.println("\nHexadecimal num1: 0x" + num1);
+        System.out.println("Hexadecimal num2: 0x" + num2);
 
 		System.out.println("\nMultiplication result in hex: 0x" + (Integer.toHexString(result)));
 	}
