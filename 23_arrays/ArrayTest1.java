17459bccfff6e81ae55b8678274d748c349515e6
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-05-28 07:29:07 +0300

    arr2

--- 23_arrays/ArrayTest1.java
+++ 23_arrays/ArrayTest1.java
@@ -1,6 +1,6 @@
 public class ArrayTest1 {
     public static void main(String[] args) {
-        boolean [] lottonumbers = new boolean[6];
+        short [] lottonumbers = new short[6];
        
         System.out.println("value = "+lottonumbers[0]);
        
