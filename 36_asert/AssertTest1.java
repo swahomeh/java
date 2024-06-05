adfb562732bda088dd89357a9355b7e820c53fb2
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-06-05 05:54:08 +0300

    assert

---
+++ 36_asert/AssertTest1.java
@@ -0,0 +1,9 @@
+public class AssertTest1 {
+    public static void main(String[] args) {
+        boolean condtion = false;
+       
+        assert condtion : "expected true condition";
+       
+        System.out.println("Finished");
+    }
+}
---
+++ 36_asert/AssertTest2.java
@@ -0,0 +1,11 @@
+public class AssertTest2 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 4;
+        //Place your code here
+        assert (a > 0) : "expected a > 0";
+        assert (b > 10) : "expected b > 10";
+
+        System.out.println("Finished");
+    }
+}
