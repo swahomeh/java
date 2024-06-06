520d66f6903a8ac0f443295ed17f8c4d21526dce
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-06-06 06:19:14 +0300

    ccs

--- 38_clean_code/MockExam2.java
+++ 38_clean_code/MockExam2.java
@@ -5,6 +5,7 @@ public class MockExam2 {
     	 int [] shoesize  =  {11,2,33,4,5,6,7,8,9,10,2,4,6,8,20,1,3,5,10,99,17,2,3,21,5,6,7,8,9,50,13,2,4,32,7,8,4,2,6,8,2,5,43,9,3,5,88,54,4,10};
 
         int n = 0;
+        System.out.println("Shoe sizes: ");
         while(n < shoesize.length){
             System.out.print(shoesize[n] +" ");
             n++;
@@ -12,66 +13,70 @@ public class MockExam2 {
         }
 
     	int j = 0;
+        System.out.println("\n\nPants Sizes: ");
     	while(j < shoesize.length){
 
-    	 	int shirt = shoesize[j];
-    	 	int pants = shoesize[j]%2;
-
+    	 	int shirt = shoesize[j]; // Shirt size == shoe size
+    	 	int pants = shoesize[j]%2; // pants size == shoesize%2 
     	 	switch(pants){
-    	 		case 0:
-    	 			System.out.println(shirt);
+    	 		case 0:    	 			
+                    System.out.print(shirt + " ");
     	 			break;
     	 	}
-    	 	 j++;
+    	 	j++;
     	}	
 
-                int t = 0;
-    	 		while(t < shoesize.length){
+        int t = 0;
+        System.out.println("\n\nSocks Sizes: ");
+ 		while(t < shoesize.length){
 
-    	 			int sock = shoesize[t];
-    	 			int shoe = shoesize[t]%11;
+ 			int sock = shoesize[t]; // sock size == shoe size
+ 			int shoe = shoesize[t]%11; // shoe size == shoesize%11
 
-    	 			switch(shoe){
-    	 			    case 0:
-    	 			 	    System.out.println(sock);
-    	 			 	    break;
-    	 			}
-    	 			t++;
-    	 		}
+ 			switch(shoe){
+ 			    case 0:
+ 			 	    System.out.print(sock + " ");
+ 			 	    break;
+ 			}
+ 			t++;
+ 		}
     	 		
         int s = 0;
+        System.out.println("\n\nAdjusted shoe Sizes (size+1): ");
         while(s < shoesize.length){
-        	System.out.print(shoesize[s] +1);
+        	System.out.print((shoesize[s] + 1) + " ");
         	s++;
         	System.out.print(" ");
         }
 
-    	    int k = 0;
-    	    while(k < shoesize.length){
+	    int k = 0;
+        System.out.println("\n\nMy shoe Sizes (size+1) % 2: ");
+	    while(k < shoesize.length){
 
-    	   		int me = shoesize[k] + 1;
-    	   		int you = me%2;
+	   		int me = shoesize[k] + 1;
+	   		int you = me%2;
 
-    	   		switch(you){
-    	   		case 0:
-    	   		     System.out.println(me);
-    	   		     break;
-    	   		}
-    	   		k++;
-    	   	}
+	   		switch(you){
+	   		case 0:
+	   		     System.out.print(me + " ");
+	   		     break;
+	   		}
+	   		k++;
+	   	}
     	   	
-    	   	int y = 0;
-    	   	while(y < shoesize.length){	
+	   	int y = 0;
+        System.out.println("\n\nOur shoe Sizes (size+1) % 11: ");
+	   	while(y < shoesize.length){	
 
-    	   		int us = shoesize[y] + 1;
-    	   		int we = us%11;
+	   		int us = shoesize[y] + 1;
+	   		int we = us%11;
 
-    	   		switch(we){
-    	   			case 0:
-    	   		      System.out.println(us);
-    	   		      break;
-    	   		}
-    	   	  y++;
-    	    }
+	   		switch(we){
+	   			case 0:
+	   		      System.out.print(us + " ");
+	   		      break;
+	   		}
+	   	  y++;
+	    }
 	}		
 }
