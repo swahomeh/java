a465cbb89bcdcf30f092771388387fa29b7d2132
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-06-11 18:35:57 +0300

    ca1

---
+++ arrays_codealong/CodeAlong1.java
@@ -0,0 +1,10 @@
+public class CodeAlong1 {
+	public static void main (String[] args) {
+		count();
+	}
+
+	public static void count () {
+		for (int i = 1; i <= 10; i++)
+			System.out.println(i);
+	}
+}
