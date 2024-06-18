8792a6e0326aa129c0e9219d1dfd08aa1003fc1e
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-06-18 13:32:09 +0300

    sd3

--- sorting_deep/SortingDeep.java
+++ sorting_deep/SortingDeep.java
@@ -19,6 +19,40 @@ public class SortingDeep {
 
 		System.out.println("\n");
 		printer(array);
+
+		System.out.println("\n");
+		quickSort(array, 0, array.length - 1);
+		printer(array);
+	}
+
+	public static void quickSort (int[] array, int low, int high) {
+		if (low < high) {
+			int pivotIndex = partition(array, low, high);
+
+			quickSort(array, low, pivotIndex - 1);
+			quickSort(array, pivotIndex + 1, high);
+		}
+	}
+
+	public static int partition (int[] array, int low, int high) {
+		int pivot = array[high];
+		int i = low - 1;
+
+		for (int j = low; j < high; j++) 
+			if (array[j] <= pivot) {
+				i++;
+				swap(array, i, j);
+			}
+
+		swap (array, i + 1, high);
+
+		return i + 1;
+	}
+
+	public static void swap (int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
 
 	public static void printer(int[] array) {
